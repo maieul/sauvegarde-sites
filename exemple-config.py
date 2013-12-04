3647fcc721c68a4ea10f626171952d09580dea47
maieul <maieul@maieul.net>
2013-12-04 22:36:04 +0100

    un exemple de config

---
+++ exemple-config.py
@@ -0,0 +1,17 @@
+dossier = "/Users/maieul/Sites/sauvegarde"
+
+sites=[
+{
+	'mode':'SSH',
+	'login':'votre login',
+	'serveur':'serveur ssh',
+	
+	'base':'emplacement sur le serveur ssh',
+	'recuperation':('IMG','tmp/dump'),
+	'exclure':('article_PDF','distant'),
+	
+	'dossier':'nom du dossier local'
+}
+       
+       
+]
